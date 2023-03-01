fa41f12669c12eb17e8be73ec2c9d3091780edeb
dimateniuta <dteniuta@gmail.com>
2023-03-01 12:42:25 +0300

    feat: add mui

---
+++ src/config/mainTheme.ts
@@ -0,0 +1,96 @@
+import { createTheme } from '@mui/material';
+
+const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#2e2e2e',
+      contrastText: '#f5f5f7',
+    },
+    secondary: {
+      main: '#c63031',
+      contrastText: '#666666',
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '::-webkit-scrollbar': {
+          width: 10,
+          height: 10,
+        },
+        '::-webkit-scrollbar-thumb': {
+          backgroundColor: '#bdbdbd',
+        },
+        '::-webkit-scrollbar-track': {
+          backgroundColor: '#f5f5f7',
+        },
+        '*': {
+          margin: 0,
+          padding: 0,
+        },
+        body: {
+          minWidth: 320,
+          margin: 0,
+        },
+        '#root': {
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          minHeight: '100vh',
+          margin: '0 auto',
+        },
+
+        header: {
+          flexGrow: 0,
+        },
+        footer: {
+          flexGrow: 0,
+        },
+        main: {
+          flexGrow: 1,
+          position: 'relative',
+          overflow: 'hidden',
+          paddingBottom: '20px',
+        },
+      },
+    },
+    MuiDialog: {
+      styleOverrides: {
+        root: {
+          minWidth: 250,
+          minHeight: 200,
+        },
+      },
+    },
+    MuiDialogTitle: {
+      styleOverrides: {
+        root: {
+          margin: 0,
+          padding: 18,
+        },
+      },
+    },
+    MuiDialogContent: {
+      styleOverrides: {
+        root: {
+          minWidth: 200,
+          minHeight: 200,
+          padding: 18,
+          overflowX: 'hidden',
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        input: {
+          '&:-webkit-autofill': {
+            WebkitBoxShadow: '0 0 0 100px #ffffff inset',
+          },
+        },
+      },
+    },
+  },
+});
+
+export default mainTheme;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css';
 import { AppProps } from 'next/app';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import mainTheme from '../config/mainTheme';
+import createEmotionCache from '../config/createEmotionCache';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+interface IAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: IAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={mainTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
